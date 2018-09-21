98e0513425ec70b3d727be280c2df1ce3e2e07a0
Philipp Trenz <mail@philipptrenz.de>
2018-09-21 14:28:23 +0200

    changed structure

--- raspberrypi/bridge.py
+++ raspberrypi/bridge.py
@@ -1,4 +1,1 @@
 #!/usr/bin/env python3
-
-from util.config import Config
-from util.database import Database
---
+++ raspberrypi/util/database.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+"""
+import sqlite3, json
+from datetime import datetime, date, time, timedelta
+
+class Database():
+
+    def __init__(self, config):
+        self.config = config
+        self.co2_mult = self.config.get_co2_avoidance_factor()
+        self.db = sqlite3.connect(self.config.get_database_path(), check_same_thread=False)
+        self.c = self.db.cursor()
+
+
+
+if __name__ == '__main__':
+    print('nothing to do here')
+
