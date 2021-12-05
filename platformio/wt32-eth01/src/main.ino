3e2cc05fe8b0b77aa7abebd34d29a7949658f694
philipptrenz <mail@philipptrenz.de>
2021-12-05 10:01:22 +0100

    fix serial reading issue, add data processing and simple web page

--- platformio/wt32-eth01/src/main.ino
+++ platformio/wt32-eth01/src/main.ino
@@ -8,7 +8,7 @@
   Licensed under MIT license
  *****************************************************************************************************************************/
 
-#define DEBUG_ETHERNET_WEBSERVER_PORT       Serial
+#define DEBUG_ETHERNET_WEBSERVER_PORT       Serial2
 
 // Debug Level from 0 to 4
 #define _ETHERNET_WEBSERVER_LOGLEVEL_       3
@@ -30,8 +30,16 @@ IPAddress mySN(255, 255, 255, 0);
 // Google DNS Server IP
 IPAddress myDNS(192, 168, 178, 1);
 
-void initializeOTA() 
-{
+
+String fetchedData = "";
+unsigned long lastFetched = millis();
+bool initiallyFetched = false;
+bool hasFechted = false;
+bool isFetchReading = false;
+bool isFetchRead = false;
+
+
+void initializeOTA() {
   // Port defaults to 3232
   ArduinoOTA.setPort(3232);
   ArduinoOTA.setHostname(HOSTNAME);
@@ -42,85 +50,107 @@ void initializeOTA()
 
   ArduinoOTA.onStart([]() {
     String type;
-    if (ArduinoOTA.getCommand() == U_FLASH)
-      type = "sketch";
-    else // U_SPIFFS
-      type = "filesystem";
-
+    if (ArduinoOTA.getCommand() == U_FLASH) type = "sketch";
+    else type = "filesystem";
     // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-    Serial.println("Start updating " + type);
-  });
-
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
   });
 
   ArduinoOTA.begin();
 }
 
-void handleRoot()
-{
-  String html = F("Hello from HelloServer running on ");
+void handlePowerMeterReading() {
+  if (!initiallyFetched || (!hasFechted && (millis() - lastFetched) > 30000)) {
+    // Fetch every 30 seconds
 
-  html += String(BOARD_NAME); 
- 
-  server.send(200, F("text/plain"), html);
+    Serial.flush();
+    Serial.write("/?!\r\n");
+
+    initiallyFetched = true;
+    hasFechted = true;
+    isFetchRead = false;
+
+    lastFetched = millis();
+
+  } else if (!isFetchRead && (millis() - lastFetched) > 10000) {
+    // Read from Serial 10 seconds after fetch
+
+    if (!isFetchReading) {
+      fetchedData = "";
+      isFetchReading = true;
+    } 
+
+    while(Serial.available() > 0) {
+      char in = Serial.read();
+
+      // Add <br> at "\n" resp. 0x0A
+      //if (in==0x0A && data.length() > 1) data = data + "<br>";
+
+      // end of message is "!" resp. 0x21, fail after 8 seconds
+      if ((fetchedData.charAt(fetchedData.length()-1) != 0x3F && in==0x21) || (millis()-lastFetched) > 8000) {
+        isFetchRead = true;
+        hasFechted = false;
+        isFetchReading = false;
+      } 
+
+      fetchedData += String(in);
+    }
+  }
 }
 
-void handlePowerMeter() {
-  String data = "";
-  String message = "";
-  message += "<html><body><h1>Verbrauchswerte</h1><p>";
+float getCounterReading(String counterId, String data) {
+  int idx2 = data.indexOf( counterId + "(");
+  if (idx2 <= 0) return -1;
 
-  Serial.println("Sending: /?!"); // start sequence
-  Serial2.write("/?!\r\n");
+  String tmp = data.substring(idx2+6);
+  int end = tmp.indexOf("*kWh)");
+  String sub = tmp.substring(0, end);
 
-  /*
-  Serial2.write(0x2F);
-  Serial2.write(0x3F);
-  Serial2.write(0x21);
-  Serial2.write(0x0D);
-  Serial2.write(0x0A);
-  */
+  return sub.toFloat();
+}  
 
-  unsigned long t_start = millis();
-  bool isInitial = true;
+void handleRoot() {
+  String message ="<html>";
 
-  while(true) {
+  message += "<head>";
+  message += "<title>Stromzähler</title>";
+  message += "<meta charset=\"utf-8\">";
+  message += "<meta name=\"viewport\" content=\"width=device-width,initial-scale=1\">";
+  message += "<meta http-equiv=\"refresh\" content=\"10; URL=http://" + myIP.toString() + "/\">";
+  message += "</head>";
 
-    char in = Serial2.read();
-    
-    if (in==0x0A) data = data + "<br>";
-    if (in==0x21 || (millis()-t_start) > 8000) break; // end of message is "!" resp. 0x21
-    if (in==0xff) continue;
+  message += "<body><h1>Stromzähler</h1>";
 
-    Serial.print(in, HEX);
-    data += String(in);
+  String data = fetchedData;
+
+  if (data.length() > 0) {
+
+    float purchase = getCounterReading("1.8.0", data);
+    if (purchase > 0)
+      message += "<p><b>Bezug:</b> " + String(purchase) + " kWh</p>";
+
+
+    float feed = getCounterReading("2.8.0", data);
+    if (feed > 0)
+      message += "<p><b>Einspeisung:</b> " + String(feed) + " kWh</p>";
+
+
+    data.replace("\n", "<br>");    
+    message += "<details><summary><b>Datagramm</b></summary><p style=\"padding-left: 16px;\">" + data + "</p></details>";
+
+  } else {
+    message += "<p><b>Noch keine Daten empfangen</b></p>";
   }
 
-  Serial.println();
 
-  message += data;
-  message += "</p></body></html>";
+  message += "<p>Vor " + String((millis() - lastFetched) / 1000) + " Sekunden aktualisiert</p>";
+  
+  message += "</body>";
+
+  message += "</html>";
   server.send(200, "text/html", message);
-
 }
 
-void handleNotFound()
-{
+void handleNotFound() {
   String message = F("File Not Found\n\n");
   
   message += F("URI: ");
@@ -139,27 +169,14 @@ void handleNotFound()
 }
 
 void setup(void) {   
-  // Serial is used for debugging and monitoring, if not using espota
-  Serial.begin(115200);
 
-  // Wait until initialized
-  while (!Serial && (millis() < 3000));
-
-  // Serial2 is connected to IR-Schreib-Lesekopf
+  // Serial is connected to IR-Schreib-Lesekopf
   // Configuration for power meter Itron ACE3000 Type 260
   // See: https://wiki.volkszaehler.org/hardware/channels/meters/power/edl-ehz/itron_ace3000_type_260 
-  Serial2.begin(300, SERIAL_7E1);
+  Serial.begin(300, SERIAL_7E1);
 
   // Wait until initialized
-  while (!Serial2);  
-
-  // Initalizing D0 interface on power meter
-  //Serial.println("\nInitializing power meter Itron ACE3000 by sending sequence: '/?!'"); // start sequence
-  //Serial2.write("/?!\r\n");
-  
-  Serial.print("\nStarting Server on " + String(ARDUINO_BOARD));
-  Serial.println(" with " + String(SHIELD_TYPE));
-  Serial.println(WEBSERVER_WT32_ETH01_VERSION);
+  while (!Serial);
 
   // To be called before ETH.begin()
   WT32_ETH01_onEvent();
@@ -177,23 +194,18 @@ void setup(void) {
 
   server.on(F("/"), handleRoot);
 
-  server.on(F("/pm"), handlePowerMeter);
-
   server.on(F("/inline"), []() {
     server.send(200, F("text/plain"), F("This works as well"));
   });
 
   server.onNotFound(handleNotFound);
-
   server.begin();
-
-  Serial.print(F("HTTP EthernetWebServer is @ IP : "));
-  Serial.println(ETH.localIP());
 
   initializeOTA();
 }
 
 void loop(void) {
+  handlePowerMeterReading();
   server.handleClient();
   ArduinoOTA.handle();
 }
