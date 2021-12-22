1136fd7713aca9ddf659283f4041312eee27e02f
Philipp Trenz <mail@philipptrenz.de>
2021-12-22 15:49:07 +0100

    Update s0-bridge.py

--- s0-bridge.py
+++ s0-bridge.py
@@ -38,7 +38,6 @@ class S0_Bridge:
                     self.cfg.log('collecting new data')
                     self.collect_data(self.db, ts)
                 t.sleep(1)
-
         except KeyboardInterrupt:
             print('Shutting down')
         except Exception as e:
@@ -49,25 +48,59 @@ class S0_Bridge:
             sys.exit(1)
 
 
-    def collect_data(self, db, ts):
+    def dry_run(self, cycles=1):
+
+        self.cfg.log('starting dry run with 5 seconds sleep after each run')
+
+        # initialize
+        self.cfg.log('adding inverters')
+        self.db.add_inverters()
+
+        self.ntwrk.process_consumption(db=self.db, cfg=self.cfg)
+
+        self.cfg.log('doing ' + str(cycles) + ' dry runs:')
+        try:
+            for _ in range(cycles):
+                ts = datetime.now().timestamp()
+                # before minute of time is a multiple of 5 minutes
+                if (int(ts) % 300) == 299:
+                    self.cfg.log('collecting new data')
+                    self.collect_data(self.db, ts, dry=True)
+                t.sleep(5)
+        except KeyboardInterrupt:
+            print('Shutting down')
+        except Exception as e:
+            print('Error occured', e)
+        finally:
+            self.db.close()
+            self.ser.close()
+            sys.exit(1)
+
+
+    def collect_data(self, db, ts, dry=False):
         ts_log = self.roundup(ts)
 
         # serial data
         if self.ser.is_enabled:
             new_serial_data = self.ser.get_power_since_last_request()
             if len(new_serial_data) > 0:
-                db.add_data(ts_log, new_serial_data)
-                self.cfg.log('added pv data from serial interface')
+                if dry: self.cfg.log("serial data:", new_serial_data)
+                if not dry: 
+                    db.add_data(ts_log, new_serial_data)
+                    self.cfg.log('added pv data from serial interface')
 
         if self.ntwrk.is_enabled:
             new_network_data = self.ntwrk.get_power_since_last_request()
+            if dry: self.cfg.log("network data:", new_network_data)
             if len(new_network_data) > 0:
-                db.add_data(ts_log, new_network_data)
-                self.cfg.log('added pv data from network interfaces')
+                if not dry: 
+                    db.add_data(ts_log, new_network_data)
+                    self.cfg.log('added pv data from network interfaces')
 
-            added = self.ntwrk.process_consumption(db=db, cfg=self.cfg)
-            if added:
-                self.cfg.log('added consumption data from network interfaces')
+            if not dry:
+                added = self.ntwrk.process_consumption(db=db, cfg=self.cfg)
+                if added:
+                    self.cfg.log('added consumption data from network interfaces')
 
         # print(ts_log, '\t', 'watts:', new_serial_data[0]['watts'], ', power:', new_serial_data[0]['power'], '\t', datetime.now())
 
@@ -79,4 +112,11 @@ class S0_Bridge:
 if __name__ == '__main__':
 
     bridge = S0_Bridge()
-    bridge.start()
+
+    if len(sys.argv) > 1 and sys.argv[1] == 'dry':
+
+        bridge.dry_run(cycles=3)
+
+    else:
+
+        bridge.start()
